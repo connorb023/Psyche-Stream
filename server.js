ad041f6c427f8bf16581fd3f6b93265610d7900b
connorb023 <connorcb@yahoo.com>
2023-04-11 13:49:28 -0600

    port and server start

--- server.js
+++ server.js
@@ -9,3 +9,9 @@ const mongoose = require ('mongoose');
 const app = express();
 const db = mongoose.connection;
 const passport = require('passport'); 
+
+
+
+// start server
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Server running on port ${port}`));
