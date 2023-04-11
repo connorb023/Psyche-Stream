36c2fed372570f2f1c831eadfde1c8edfe3dfd4f
connorb023 <connorcb@yahoo.com>
2023-04-11 13:52:06 -0600

    mongo code

--- server.js
+++ server.js
@@ -2,13 +2,30 @@ require('dotenv').config()
 
 
 //Dependencies
-//___________________
 const express = require('express');
 const methodOverride = require('method-override');
 const mongoose = require ('mongoose');
 const app = express();
 const db = mongoose.connection;
 const passport = require('passport'); 
+
+//Middleware
+// Connect to the database either via heroku or locally
+const MONGODB_URI = process.env.MONGODB_URI;
+
+// Connect to Mongo &
+// Fix Depreciation Warnings from Mongoose
+// May or may not need these depending on your Mongoose version
+mongoose.connect(MONGODB_URI , { useNewUrlParser: true, useUnifiedTopology: true }
+);
+
+// Error / success
+db.on('error', (err) => console.log(err.message + ' is mongod not running?'));
+db.on('connected', () => console.log('mongod connected: ', MONGODB_URI));
+db.on('disconnected', () => console.log('mongod disconnected'));
+
+
+
 
 
 
