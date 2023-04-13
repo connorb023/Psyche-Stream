633524b3141c423854628c3159d5edf603aa6222
connorb023 <connorcb@yahoo.com>
2023-04-13 11:23:42 -0600

    updates

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost:27017/moodtrack
 
 // Error / success
 db.on('error', (err) => console.log(err.message + ' is mongo not running?'));
-db.on('connected', () => console.log('mongo connected: ', MONGODB_URI));
+db.on('connected', () => console.log('mongo connected: '));
 db.on('disconnected', () => console.log('mongo disconnected'));
 
 // define Mood schema
