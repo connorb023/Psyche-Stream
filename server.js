54f6ce75c75de51fae8019652626762227bb6d63
connorb023 <connorcb@yahoo.com>
2023-04-13 22:40:36 -0600

    mood tracking form

--- server.js
+++ server.js
@@ -48,7 +48,11 @@ Mood.find().sort('-date').exec()
 });
 
   });
+  app.get('/login', (req, res) => {
+    res.render('login');
+  });
   
+
 // Start server
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Server running on port ${port}`));
